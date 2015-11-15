fce5b81863fadf947edd1019db681419e6af3bf8
Maria D. Kelly <maria.d.kelly@gmail.com>
2015-11-15 14:59:15 -0500

    - Remove unused users routes. Add some layout and styles.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	title: 'PriceBook',
+  	items: ["one", "two", "three"]
+  });
 });
 
 module.exports = router;
